f34af3e618476e71e3f15dccd025ae91ae2206e7
RethtihpongEM <rithtipongem@gmail.com>
2024-01-24 13:32:52 +0700

    finished

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Todo } from 'src/app/ts/interfaces/todo.interfaces';
 
 @Component({
@@ -6,12 +6,19 @@ import { Todo } from 'src/app/ts/interfaces/todo.interfaces';
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-export class TodoItemComponent implements OnInit{
+export class TodoItemComponent  {
   @Input({required:true}) todo?: Todo;
   @Input({required:true}) index?: number
 
-  ngOnInit(): void {
-    console.log(this.todo,this.index)
+  @Output() completeActionBtn = new EventEmitter<number>();
+  @Output() deleteTaskBtn = new EventEmitter<number>();
+
+  completeTask():void {
+    this.completeActionBtn.emit(this.index)
+  }
+
+  deleleTask():void {
+    this.deleteTaskBtn.emit(this.index)
   }
 
 }
--- src/app/pages/todo-view/todo-view.component.ts
+++ src/app/pages/todo-view/todo-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/ts/interfaces/todo.interfaces';
 import {v4 as uuidv4} from 'uuid'
 
@@ -7,36 +7,42 @@ import {v4 as uuidv4} from 'uuid'
   templateUrl: './todo-view.component.html',
   styleUrls: ['./todo-view.component.css']
 })
-export class TodoViewComponent {
-  todoList: Todo [] = [
-    {
-      id: '1asdfasf',
-      todo: "Do homework",
-      isCompleted: true,
-      isEditing: false,
-    },
-    {
-      id: 'sadf13',
-      todo: "Complete UI",
-      isCompleted: false,
-      isEditing: false,
-    },
-    {
-      id: 'asdfas21',
-      todo: "Send to database",
-      isCompleted: false,
-      isEditing: false,
-    },
-  ]
+export class TodoViewComponent{
+  todoList: Todo [] = []
+
+  constructor() {
+    this.getTodos()
+  }
+
+  getTodos (){
+    this.todoList = JSON.parse(localStorage.getItem('todos') || '[]')
+  }
+
+  deleteTask($event: number) {
+    this.todoList.splice($event,1)
+    this.updateTodoList()
+    this.getTodos();
+  }
+
+  completeTask($event: number) {
+    this.todoList[$event].isCompleted = !this.todoList[$event].isCompleted
+    this.updateTodoList()
+    this.getTodos();
+  }
+
+  updateTodoList (){
+    localStorage.setItem("todos", JSON.stringify(this.todoList))
+  }
 
   createTodo ($event: string) {
-    this.todoList.push({
+    const tempTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+    tempTodos.push({
       id: uuidv4(),
       todo: $event,
       isCompleted:false,
       isEditing: false
     })
+    localStorage.setItem("todos", JSON.stringify(tempTodos))
+    this.getTodos()
   }
-
-
 }
